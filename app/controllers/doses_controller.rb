b3669cb0175de1f97367e34e4b6185069bf24592
Paphada Eawsivigoon <paphadaeawsivigoon@gmail.com>
2019-03-28 17:45:24 +0900

    complete backend

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,33 @@
+class DosesController < ApplicationController
+  before_action :find_cocktail, only: [:new, :create]
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    if @dose.save
+      redirect_to cocktail_path(params[:cocktail_id])
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail_id)
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:ingredient_id, :description)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'JSON'
+
+Ingredient.destroy_all
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+json = open(url).read
+ingredients = JSON.parse(json)
+ingredients['drinks'].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
